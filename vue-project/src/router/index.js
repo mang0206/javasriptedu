8e32800164432da4249284ca4e74b4d1e5774745
mang0206 <alswosp0206@naver.com>
2023-11-10 15:33:37 +0900

    Add pinia files

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -4,6 +4,7 @@ import Home from '@/pages/Home.vue'
 import StudyView1 from '@/pages/StudyView1.vue'
 import StudyView2 from '@/pages/StudyView2.vue'
 import StudyView3 from '@/pages/StudyView3.vue'
+import StudyView4 from '@/pages/StudyView4.vue'
 import WorkView from '@/pages/WorkView.vue'
 
 const router = createRouter({
@@ -13,6 +14,7 @@ const router = createRouter({
         { path: '/Study1', component: StudyView1 },
         { path: '/Study2', component: StudyView2 },
         { path: '/Study3', component: StudyView3 },
+        { path: '/Study4', component: StudyView4 },
         { path: '/Work', component: WorkView },
     ]
 })
