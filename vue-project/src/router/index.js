ad1a38a97016565b1e8e312f7c1291fe25a57181
mang0206 <alswosp0206@naver.com>
2023-11-20 09:15:14 +0900

    Study jwt connection example

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -5,6 +5,8 @@ import StudyView1 from '@/pages/StudyView1.vue'
 import StudyView2 from '@/pages/StudyView2.vue'
 import StudyView3 from '@/pages/StudyView3.vue'
 import StudyView4 from '@/pages/StudyView4.vue'
+import StudyView5 from '@/pages/StudyView5.vue'
+import StudyView6 from '@/pages/StudyView6.vue'
 import WorkView from '@/pages/WorkView.vue'
 
 const router = createRouter({
@@ -15,6 +17,8 @@ const router = createRouter({
         { path: '/Study2', component: StudyView2 },
         { path: '/Study3', component: StudyView3 },
         { path: '/Study4', component: StudyView4 },
+        { path: '/Study5', component: StudyView5 },
+        { path: '/Study6', component: StudyView6 },
         { path: '/Work', component: WorkView },
     ]
 })
